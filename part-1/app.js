7688682dc5bfc43f5359eefc07a9e57caf1fab95
bj bautista <emailme@djbrigidope.com>
2017-06-20 03:21:54 -0700

    /zero route

---
+++ part-1/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/zero',(req,res) =>{
+  res.setHeader('Content-Type', 'text/plain');
+  res.setStatus = 200;
+  res.send('zero');
+})
+
+
+//call back consoles an error message jik server crashes before the console.log
+app.listen(port, (err,res)=>{
+  if(err){
+    console.log("err on tha srrver bruh");
+  }else{
+    console.log("gthang hot on port 8thow wow");
+  }
+});
+// app.listen(console.log("highthere"));
