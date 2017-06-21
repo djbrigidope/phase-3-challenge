e5d364471b379a4cf2d497c33d9053100cc3572d
bj bautista <emailme@djbrigidope.com>
2017-06-21 06:46:48 -0700

    morning commit. working phase 3 but lo ciento it's no bueno

---
+++ part-3/index.js
@@ -0,0 +1,29 @@
+
+const cart = [];
+let item = (name, price, count)=>{
+  this.name=name;
+  this.price=price;
+  this.count=count;
+};
+
+
+//addItemToCart
+
+let addItemToCart=(name, price, count)=>{
+  let item = new item(name, price, count);
+  cart.push(item);
+}
+
+//removeItemFromCart
+
+//removeItemFromCartAll
+
+// clearCart()
+
+//countCart()
+
+//totalCart()
+
+//saveCart()
+
+//loadCart()
